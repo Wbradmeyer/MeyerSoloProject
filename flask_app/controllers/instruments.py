726a64c99d929fabdf9d0a47997aa5967c6a00ac
W. Brad Meyer <wbradmeyer@gmail.com>
2025-04-11 11:47:04 -0400

    use imported wraps to protect routes via login

--- flask_app/controllers/instruments.py
+++ flask_app/controllers/instruments.py
@@ -1,4 +1,5 @@
 import os
+from functools import wraps
 from flask_app import app
 from flask import render_template, redirect, request, session, url_for
 from werkzeug.utils import secure_filename
@@ -9,10 +10,18 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            return redirect('/')
+        return f(*args, **kwargs)
+    return decorated_function
+
 # Create Instruments Controller
 @app.route('/instruments/new', methods=['POST', 'GET'])
+@login_required
 def create_instrument():
-    if 'user_id' not in session: return redirect('/') # *** route protection, must be logged in
     if request.method == 'POST':
         # create a new instrument and retrieve the id
         instrument_id = instrument.Instrument.create_new_instrument(request.form)
@@ -30,16 +39,15 @@ def create_instrument():
 
 # Read Instruments Controller
 @app.route('/home')
+@login_required
 def show_user_instruments():
-    if 'user_id' not in session: return redirect('/') # ***
-    # Query the db passing in session user id to return the list of user instruments
     instruments = instrument.Instrument.get_user_instruments(session['user_id'])
     return render_template('home.html', instruments=instruments)
 
 
 @app.route('/instruments/all/', methods=['POST', 'GET'])
+@login_required
 def show_all_instruments():
-    if 'user_id' not in session: return redirect('/') # ***
     instruments_for_sale = instrument.Instrument.get_all_instruments_for_sale()
 
     if request.method == "POST":
@@ -57,8 +65,8 @@ def show_all_instruments():
 
 
 @app.route('/instruments/<int:id>')
+@login_required
 def instrument_card(id):
-    if 'user_id' not in session: return redirect('/') # ***
     this_instrument = instrument.Instrument.get_instrument_by_id(id)
     return render_template('one_instrument.html', instrument = this_instrument)
 
@@ -70,8 +78,8 @@ def get_instrument_image(filename):
 
 # Update Instruments Controller
 @app.route('/instruments/edit/<int:id>', methods=['POST', 'GET'])
+@login_required
 def edit_instrument(id):
-    if 'user_id' not in session: return redirect('/') # ***
     if request.method == 'POST':
         updated = instrument.Instrument.update_instrument(request.form)
         if updated:
@@ -95,16 +103,16 @@ def edit_instrument(id):
 
 
 @app.route('/instruments/purchase/<int:id>', methods=['POST'])
+@login_required
 def purchase_instrument(id):
-    if 'user_id' not in session: return redirect('/') # ***
     instrument.Instrument.change_owner(id)
     return redirect('/home')
 
 
 # Delete Instruments Controller
 @app.route('/instruments/delete/<int:id>')
+@login_required
 def delete_instrument(id):
-    if 'user_id' not in session: return redirect('/') # ***
     if instrument.Instrument.delete_instrument_by_id(id):
         return redirect('/home')
     else:
